db4e13251c72a25e3d41a0fd77af663e49636433
rokas <rokasra@gmail.com>
2022-05-23 10:16:02 +0300

    ejs install, some calculations

--- src/server.js
+++ src/server.js
@@ -4,18 +4,23 @@ const path = require('path');
 
 const PORT = 3007;
 
+// Set default view engine default view directory /views
+app.set('view engine', 'ejs');
+// kelias iki views direktorijos nuo root
+app.set('views', 'src/views');
+
 // static directory
 const staticDir = path.join(__dirname, 'assets');
 app.use(express.static(staticDir));
 
 app.get('/', function (req, res) {
-  const pathToIndex = path.join(__dirname, 'views', 'index.html');
-  res.sendFile(pathToIndex);
+  //   const pathToIndex = path.join(__dirname, 'views', 'index.html');
+  //   res.sendFile(pathToIndex);
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  const pathToAbout = path.join(__dirname, 'views', 'about.html');
-  res.sendFile(pathToAbout);
+  res.render('about');
 });
 
 app.listen(PORT, () => console.log('listening on port', PORT));
