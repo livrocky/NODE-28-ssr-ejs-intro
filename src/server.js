d5acd405151182b60cd1154a4afdb1e15cd9c277
rokas <rokasra@gmail.com>
2022-05-23 09:41:21 +0300

    static dir, css, img

--- src/server.js
+++ src/server.js
@@ -4,9 +4,18 @@ const path = require('path');
 
 const PORT = 3007;
 
+// static directory
+const staticDir = path.join(__dirname, 'assets');
+app.use(express.static(staticDir));
+
 app.get('/', function (req, res) {
   const pathToIndex = path.join(__dirname, 'views', 'index.html');
   res.sendFile(pathToIndex);
+});
+
+app.get('/about', (req, res) => {
+  const pathToAbout = path.join(__dirname, 'views', 'about.html');
+  res.sendFile(pathToAbout);
 });
 
 app.listen(PORT, () => console.log('listening on port', PORT));
