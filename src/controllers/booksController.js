698327eef07e816eae25fa2b34603fa5d0b98dce
rokas <rokasra@gmail.com>
2022-05-25 11:10:48 +0300

    books controller

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -1,7 +1,7 @@
+/* eslint-disable object-curly-newline */
 /* eslint-disable one-var */
 /* eslint-disable one-var-declaration-per-line */
-
-const { getAllBooks } = require('../model/bookModel');
+const { getAllBooks, addBook } = require('../model/bookModel');
 
 const showBooks = async (req, res) => {
   console.log('req.body===', req.body);
@@ -23,6 +23,30 @@ const showBooks = async (req, res) => {
   res.render('books', locals);
 };
 
+const showNewBooks = async (req, res) => {
+  const locals = {
+    currentPage: 'new-form',
+    title: 'Lets create a new book',
+  };
+  res.render('new-book', locals);
+};
+
+const postNewBooks = async (req, res) => {
+  console.log('req.body===', req.body);
+  const { title, author, year, category } = req.body;
+  const result = await addBook(title, author, year, category);
+  console.log('result===', result);
+  const feedback = result.affectedRows === 1 ? 'Success' : 'Error';
+  const locals = {
+    currentPage: 'new-form',
+    title: 'Lets create a new book',
+    feedback,
+  };
+  res.render('new-book', locals);
+};
+
 module.exports = {
   showBooks,
+  showNewBooks,
+  postNewBooks,
 };
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,7 +3,6 @@
 /* eslint-disable one-var */
 /* eslint-disable one-var-declaration-per-line */
 const express = require('express');
-const { addBook } = require('../model/bookModel');
 const controller = require('../controllers/booksController');
 const bookRoutes = express.Router();
 
@@ -12,26 +11,8 @@ bookRoutes.get('/books', controller.showBooks);
 
 // GET /new-book - render new-book.ejs kuriame yra forma sukurti naujai knygai
 
-bookRoutes.get('/books/new', async (req, res) => {
-  const locals = {
-    currentPage: 'new-form',
-    title: 'Lets create a new book',
-  };
-  res.render('new-book', locals);
-});
+bookRoutes.get('/books/new', controller.showNewBooks);
 
-bookRoutes.post('/books/new', async (req, res) => {
-  console.log('req.body===', req.body);
-  const { title, author, year, category } = req.body;
-  const result = await addBook(title, author, year, category);
-  console.log('result===', result);
-  const feedback = result.affectedRows === 1 ? 'Success' : 'Error';
-  const locals = {
-    currentPage: 'new-form',
-    title: 'Lets create a new book',
-    feedback,
-  };
-  res.render('new-book', locals);
-});
+bookRoutes.post('/books/new', controller.postNewBooks);
 
 module.exports = bookRoutes;
